0a8737354fb37d61c6bcb89ed8b91e1f5682e5f9
Gary <garycraft@our-space.xyz>
2026-01-08 12:22:00 -0600

    Update company name and copyright in build.rs

--- build.rs
+++ build.rs
@@ -4,9 +4,9 @@ fn main() {
         let mut res = winres::WindowsResource::new();
         res.set("FileDescription", "HTTP Discovery Service");
         res.set("ProductName", "HTTP Discovery Service");
-        res.set("CompanyName", "Cosmic Knowledge");
+        res.set("CompanyName", "spacedock-zero");
         res.set("OriginalFilename", "http-discovery-service.exe");
-        res.set("LegalCopyright", "Copyright (c) 2026 Cosmic Knowledge");
+        res.set("LegalCopyright", "Copyright (c) 2026 spacedock-zero");
         // We can also set an icon if one existed
         // res.set_icon("icon.ico");
         res.compile().unwrap();
