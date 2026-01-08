f29fbb05564e6b1cabd28c1b07ba5970f41876cc
Gary <garycraft@our-space.xyz>
2026-01-08 12:22:32 -0600

    Cleanup
    Removed comment about setting an icon in build script.

--- build.rs
+++ build.rs
@@ -7,7 +7,6 @@ fn main() {
         res.set("CompanyName", "spacedock-zero");
         res.set("OriginalFilename", "http-discovery-service.exe");
         res.set("LegalCopyright", "Copyright (c) 2026 spacedock-zero");
-        // We can also set an icon if one existed
         // res.set_icon("icon.ico");
         res.compile().unwrap();
     }
